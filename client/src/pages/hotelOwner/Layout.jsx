d9a1f6b0f53c5f8b2f56e5970e8e5f46397df56d
sdaabimanyu <sdaabimanyu@gmail.com>
2026-06-01 17:52:51 +0530

    fix: correct import path for NavBar component in Layout.jsx

--- client/src/pages/hotelOwner/Layout.jsx
+++ client/src/pages/hotelOwner/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import NavBar from "../../components/hotelOwner/NavBar";
+import NavBar from "../../components/hotelOwner/Navbar";
 import Sidebar from "../../components/hotelOwner/Sidebar";
 import { Outlet } from "react-router-dom";
 import { useAppContext } from "../../context/AppContext";
