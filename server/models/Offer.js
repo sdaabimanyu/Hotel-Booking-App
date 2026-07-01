7c59eb8aed5903845403090b905ac6332d0b8864
sdaabimanyu <sdaabimanyu@gmail.com>
2026-07-01 11:40:15 +0530

    feat: change createdBy field type from ObjectId to String in offer schema

--- server/models/Offer.js
+++ server/models/Offer.js
@@ -64,7 +64,7 @@ const offerSchema = new mongoose.Schema(
     },
 
     createdBy: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: String,
       ref: "User",
       required: true,
     },
